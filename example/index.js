3599e753073f704991a1470912898b09e129a3da
熊杰 <285516193@qq.com>
2018-02-01 15:35:56 +0800

    例子初步

--- example/index.js
+++ example/index.js
@@ -2,8 +2,15 @@ import Crop from '../src/index';
 import imgSrc from './img.jpg';
 
 const crop = new Crop({
-    selectot: '#cvs',
-    imgSrc
+    selectot: '#cvs'
 });
-
-console.log(crop);
+function pathToBase() {
+    return new Promise(resolve => {
+        const image = new Image();
+        image.onload = function() {
+            resolve(image);
+        };
+        image.src = imgSrc;
+    });
+}
+crop.setImg(pathToBase);
