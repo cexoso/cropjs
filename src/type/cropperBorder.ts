872701ad205d050658ef3a2ef552cb36d4a74e2f
熊杰 <285516193@qq.com>
2018-02-06 16:47:06 +0800

    裁剪框完成

--- src/type/cropperBorder.ts
+++ src/type/cropperBorder.ts
@@ -7,7 +7,16 @@ export default class CropperBorder extends DrawerLayer {
         super(canvas)
         this.drawBorder(options)
     }
-    private drawBorder(opts) {
-        console.log(opts);
+    private drawBorder(opts: { border: number, left: number, top: number, size: number }) {
+        this.ctx.fillStyle = "#1a1a1bc2";
+        this.ctx.fillRect(0, 0, this.clientWidth, this.clientHeight);
+        const w = this.clientWidth * opts.size;
+        const h = w / opts.border
+        this.ctx.clearRect(
+            this.clientWidth * opts.left - w / 2,
+            this.clientHeight * opts.top - h / 2,
+            w,
+            h,
+        )
     }
 }
