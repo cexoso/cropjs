a4df21308529c735fd1d9fb05f0d396364117425
熊杰 <285516193@qq.com>
2018-02-07 16:13:15 +0800

    修改类型替换mitt

---
+++ src/mitt.ts
@@ -0,0 +1,18 @@
+export type EventHandle = (event?: any) => any
+export default class Mitt {
+    private all: {[name: string]: EventHandle[]} 
+    public on(type: string, handler: EventHandle) {
+        (this.all[type] || (this.all[type] = [])).push(handler);
+    }
+    public off(type: string, handler: EventHandle) {
+        if (this.all[type]) {
+            const indexOf = this.all[type].indexOf(handler)
+            if (indexOf) {
+                this.all[type].splice(indexOf, 1);
+            }
+        }
+    }
+    public emit(type: string, evt: any) {
+        (this.all[type] || []).map((handler) => { handler(evt) });
+    }
+}
--- src/preview.ts
+++ src/preview.ts
@@ -1,16 +1,17 @@
 
 export default class Preview {
     private canvas: HTMLCanvasElement
+    private ctx: CanvasRenderingContext2D
     constructor(imageData: ImageData) {
         this.canvas = document.createElement('canvas')
         this.canvas.width = imageData.width
         this.canvas.height = imageData.height
-        const ctx = this.canvas.getContext('2d')
-        ctx.putImageData(imageData, 0, 0);
+        this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D
+        this.ctx.putImageData(imageData, 0, 0);
     }
-    public toBlob(): Promise<Blob> {
+    public toBlob(): Promise<any> {
         return new Promise((resolve) => {
-            this.canvas.toBlob(resolve)
+            this.canvas.toBlob(resolve)// todo 可能需要关心的type参数
         })
     }
     public toDataUrl(type?: string, quality?: number) {
