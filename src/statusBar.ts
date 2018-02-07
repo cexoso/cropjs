8c3956ab163df76056c4215742c94b7e5621d04e
张集伟 <Zhangjw01@mingyuanyun.com>
2018-02-07 16:14:58 +0800

    底部功能按键外包一层div,添加背景图片

--- src/statusBar.ts
+++ src/statusBar.ts
@@ -13,6 +13,7 @@ export default class StatusBar {
         fragment.appendChild(this.creataBotton('裁剪', 'crop'));
         if (options.zoom) {
             fragment.appendChild(this.creataBotton('放大', 'zoomIn'));
+            fragment.appendChild(this.creataBotton('放大', 'zoomIn'));            
         }
         div.appendChild(fragment)
     }
@@ -23,6 +24,8 @@ export default class StatusBar {
         const dom = document.createElement('span');
         dom.innerText = name
         onclick(dom, (e) => this.emitter.emit(eventName, e))
-        return dom;
+        const wrap = document.createElement('div');
+        wrap.appendChild(dom);
+        return wrap;
     }
 }
