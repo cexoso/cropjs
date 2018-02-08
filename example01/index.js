9d70c4aebfef31d339d0eb49964112059196843b
张集伟 <Zhangjw01@mingyuanyun.com>
2018-02-08 16:00:16 +0800

    修改demo样式

--- example01/index.js
+++ example01/index.js
@@ -6,32 +6,38 @@ const crop = new Crop({
 
 window.onload = function() {
     const input = document.getElementById('file');
-    input.addEventListener('change', handleInput);
-};
-
-function handleInput() {
-    const pathToBase = () => {
-        return new Promise(resolve => {
-            const image = document.createElement('img');
-            image.onload = function() {
-                showInputBox(false);
-                // setTimeout(()=>)
-                resolve(image);
-            };
-            image.src = window.URL.createObjectURL(this.files[0]);
-        });
-    };
-    crop.setImg(pathToBase);
-}
-
-function showInputBox(show) {
     const inputBox = document.getElementById('file-input');
-    const cvs = document.getElementById('cvs');
-    if (show) {
-        inputBox.style.display = 'block';
-        cvs.style.display = 'none';
-    } else {
-        inputBox.style.display = 'none';
-        cvs.style.display = 'block';
+    const esc = document.getElementById('esc');
+    esc.addEventListener('click', () => openCrop(false));
+    input.addEventListener('change', handleInput);
+    window.addEventListener('keyup', function(e) {
+        if (e.keyCode === 27) {
+            openCrop(false);
+        }
+    });
+
+    function handleInput() {
+        if (this.files[0]) {
+            const pathToBase = () => {
+                return new Promise(resolve => {
+                    const image = document.createElement('img');
+                    image.onload = function() {
+                        openCrop(true);
+                        resolve(image);
+                    };
+                    image.src = window.URL.createObjectURL(this.files[0]);
+                });
+            };
+            crop.setImg(pathToBase);
+        }
     }
-}
+
+    function openCrop(show) {
+        if (show) {
+            inputBox.style.display = 'none';
+        } else {
+            inputBox.style.display = 'block';
+            input.value = null;
+        }
+    }
+};
