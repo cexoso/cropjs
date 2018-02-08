3a27fb8635ed3501daf221c170d42ab2dc72b876
熊杰 <285516193@qq.com>
2018-02-08 17:51:01 +0800

    不再提出css

--- rollup.config.js
+++ rollup.config.js
@@ -14,9 +14,7 @@ export default {
         typescriptPlugin({
             typescript
         }),
-        postcss({
-            extract: true
-        }),
+        postcss(),
         uglify()
     ]
 };
