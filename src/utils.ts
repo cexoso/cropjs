2146737ee15d02128b5350d0c9f5569e8a1cfa28
熊杰 <285516193@qq.com>
2018-02-09 14:38:05 +0800

    添加now函数

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,6 @@
 export function onclick(dom: HTMLElement, eventHandle: (e: Event) => any) {
     dom.addEventListener('click', eventHandle)
 }
+export function now() {
+    return performance.now(); // for no imcompatible
+}
