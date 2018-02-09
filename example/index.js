120382ae1242d7be3d470cc698adc75f37096b48
熊杰 <285516193@qq.com>
2018-02-09 18:24:41 +0800

    例子修改

--- example/index.js
+++ example/index.js
@@ -1,24 +1,21 @@
 import Crop from '../src/index';
 import imgSrc from './img.jpg';
 
-const crop = new Crop({
-    selector: '#cvs'
-});
-function pathToBase() {
-    return new Promise(resolve => {
-        const image = new Image();
-        image.onload = function() {
-            resolve(image);
-        };
-        image.src = imgSrc;
-    });
-}
-crop.setImg(pathToBase);
-(function() {
-    var script = document.createElement('script');
-    script.src = '//cdn.jsdelivr.net/npm/eruda';
-    document.body.appendChild(script);
-    script.onload = function() {
-        eruda.init();
+const crop = new Crop('#cvs');
+setTimeout(() => {
+    crop.show();
+    const image = new Image();
+    image.onload = function() {
+        crop.setImg(image);
     };
-})();
+    image.src = imgSrc;
+}, 700);
+
+// (function() {
+//     var script = document.createElement('script');
+//     script.src = '//cdn.jsdelivr.net/npm/eruda';
+//     document.body.appendChild(script);
+//     script.onload = function() {
+//         eruda.init();
+//     };
+// })();
