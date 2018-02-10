5c86b3cc23c3a95c7a71659b4f99896cc9d030aa
熊杰 <285516193@qq.com>
2018-02-10 16:25:04 +0800

    更新例子

--- example/index.js
+++ example/index.js
@@ -10,6 +10,16 @@ setTimeout(() => {
     };
     image.src = imgSrc;
 }, 700);
+setTimeout(() => {
+    crop.setOptions({
+        cropOpts: {
+            border: 1.5, // 边框的长宽比 如：1.5 长宽比为1.5 : 1
+            left: 0.5, // 50%
+            size: 0.6, // 框架大小，相对于整个canvas比例 size: 0.8 canvas宽度的0.8
+            top: 0.5 // 30%
+        }
+    });
+}, 1300);
 
 // (function() {
 //     var script = document.createElement('script');
