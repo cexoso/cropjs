599504b4807cc3d102b2db53dc9908499b0a466f
熊杰 <285516193@qq.com>
2018-02-11 14:48:03 +0800

    只需要一个例子

--- example01/beautify.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    const tips = document.getElementById('file-input-tips');
-    const input = document.getElementById('file');
-    tips.onclick = function() {
-        input.click();
-    };
-};
